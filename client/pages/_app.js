ab6edc379f4900459188ee51fcedc993ce91d461
Jack Andrews <japotts16@gmail.com>
2022-08-27 01:59:36 +0100

    basic layout

---
+++ client/pages/_app.js
@@ -0,0 +1,14 @@
+import MainLayout from "../layout/main";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
+}
+
+export default MyApp;
