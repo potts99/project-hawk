53c2a57ae719d8dfb7f0324116db0ad9216db6bf
Jack Andrews <japotts16@gmail.com>
2022-08-29 22:22:18 +0100

    ui: fixes

--- client/layout/main.js
+++ client/layout/main.js
@@ -285,11 +285,15 @@ export default function MainLayout({ children }) {
                     <div>
                       <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
+                        <span className="inline-block h-6 w-6 overflow-hidden rounded-full bg-gray-100">
+                          <svg
+                            className="h-full w-full text-gray-300"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                          </svg>
+                        </span>
                       </Menu.Button>
                     </div>
                     <Transition
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,42 @@
-
-
 // Default Index Dashboard - will show various data driven graphs for global metrics
 
+const stats = [
+  { name: "Total Events", stat: "71,897" },
+  { name: "Busiest Project", stat: "Test - 440 reqs" },
+  { name: "Busiest Channel", stat: "User Sign Up - 300 reqs" },
+];
+
 export default function Index() {
-
-    return (
-        <div>
-
+  return (
+    <div className="">
+      <header>
+        <div className="py-4">
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+            Welcome to Project Hawk
+          </h1>
         </div>
-    )
+      </header>
+
+      <div>
+        <h3 className="text-lg font-medium leading-6 text-gray-900">
+          Last 30 days
+        </h3>
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          {stats.map((item) => (
+            <div
+              key={item.name}
+              className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+            >
+              <dt className="truncate text-sm font-medium text-gray-500">
+                {item.name}
+              </dt>
+              <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+                {item.stat}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
+  );
 }
